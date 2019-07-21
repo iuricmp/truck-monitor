526f32e065ab33ac113a86333d73e927bfd6e86e
Iuri Pereira <iuri.pereira@talkdesk.com>
2019-07-21 11:18:25 +0100

    Renders the Layout component by default

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { act } from 'react-dom/test-utils'
 import App from './App'
+import axios from 'axios'
+import MockAdapter from 'axios-mock-adapter'
+
+const mock = new MockAdapter(axios)
+
+beforeEach(() => {
+  mock.onGet('/vehicles/lastPosition').reply(200, {
+    vehicles: [{ id: 1, licensePlate: 'AVIB9393', position: [51.4912, 0.15] }]
+  })
+})
 
 it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
-  ReactDOM.unmountComponentAtNode(div)
+  act(() => {
+    const div = document.createElement('div')
+    ReactDOM.render(<App />, div)
+    ReactDOM.unmountComponentAtNode(div)
+  })
 })
